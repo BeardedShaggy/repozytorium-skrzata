3c307ed41af4cbd41f343ad257e06a7300caefbd
Bonefacey <55958468+BeardedShaggy@users.noreply.github.com>
2020-08-20 01:30:53 +0200

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Liczba liczb w działaniu");
+            int i= Convert.ToInt32(Console.ReadLine()); //liczba ile liczb bedzie?
+            int wynik = Convert.ToInt32(wynik = 1);
+            while (i-- > 0)
+            {
+                Console.WriteLine("Podaj Liczbe przez która chcesz pomnozyc");
+                int liczba1 = Convert.ToInt32(Console.ReadLine());
+                //Console.WriteLine(wynik * liczba1); oups zauwazylem ze to nie do konca dziala
+                wynik = wynik * liczba1;
+            }
+            Console.WriteLine("Wynik to:");
+            Console.WriteLine(wynik);
         }
     }
 }
+   
