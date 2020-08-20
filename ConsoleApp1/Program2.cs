4d520c06900ef696b81d432c72d1629b9ef22d20
Bonefacey <55958468+BeardedShaggy@users.noreply.github.com>
2020-08-20 22:11:28 +0200

    Update Program2.cs

--- ConsoleApp1/Program2.cs
+++ ConsoleApp1/Program2.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Liczba liczb w działaniu");
             int i = Convert.ToInt32(Console.ReadLine()); //liczba ile liczb bedzie?
-            int wynik = Convert.ToInt32(wynik = 0); //Wynik = 1 działa tylko przy mnożeniu :(
+            int wynik = Convert.ToInt32(wynik = 0); //Wynik = 1 działa tylko przy mnożeniu i dzieleniu :(
             while (i-- > 0)
             {
               Console.WriteLine("podaj znak"); 
@@ -20,31 +20,36 @@ namespace ConsoleApp1
                 switch (znak)
                 {
                     case "+":
+
                         wynik = wynik + liczba1;
                         break;
                     case "-":
+
                         wynik = wynik - liczba1;
                         break;
                     case "*":
 
-                        if (wynik == 0) //nie prawidłowo zrobione
+                        if (wynik == 0)
                         {
                             wynik++;
                         }
-
+                        
                         wynik = wynik * liczba1;
                         break;
+                    
                     case "/":
 
-                        if (wynik == 0) //nie prawidłowo zrobione
+                        if (wynik == 0)
                         {
                             wynik++;
                         }
+                        
                         wynik = wynik / liczba1;
                         break;
-                        /*else:                       //do poprawy
-                            Console.WriteLine("nie prawidłowy znak");
-                            break; */
+
+                     default:                     //default - nie else.
+                            Console.WriteLine("Nie prawidłowy znak");
+                            break; 
                 }
                 //Console.WriteLine(wynik * liczba1); oups zauwazylem ze to nie do konca dziala
 
