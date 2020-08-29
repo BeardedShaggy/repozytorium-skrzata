c03defe286b80898ff0c6102ff0ae5135f0c71bf
Bonefacey <55958468+BeardedShaggy@users.noreply.github.com>
2020-08-30 01:24:47 +0200

    Create menu.cs

---
+++ ConsoleApp1/menu.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zadania
+{
+    class menu
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz numer zadania");
+            int zadanie = Convert.ToInt32(Console.ReadLine());
+            switch (zadanie)
+            {
+                case 1:
+
+                    Zadania.Obliczanie.zadanie1(args); // namespace.class.method()
+
+                    break;
+                
+                case 2:
+
+                    Zadania.Obliczanie2.Zadanie2(args);
+
+                    break;
+               
+                case 3:
+
+                    Zadania.Obliczanie3.Zadanie3(args);
+
+                    break;
+                
+                case 4:
+
+                    Zadania.Obliczanie4.zadanie4(args);
+
+                    break;
+
+            }
+
+        }
+    }
+}
