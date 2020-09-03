99bee915a072fd8b9e44c4dd5979382a0a8ddc64
Bonefacey <55958468+BeardedShaggy@users.noreply.github.com>
2020-09-03 15:51:12 +0200

    Poprawiono błędy, dodano co musze dodać :D

--- ConsoleApp1/KiK.cs
+++ ConsoleApp1/KiK.cs
@@ -11,7 +11,17 @@ namespace Zadania
         {
 
             string[,] array = new string[3, 3]; //3 na 3 kratka.
-                System.Console.WriteLine("");
+            array.SetValue("1", 0, 0);
+            array.SetValue("2", 0, 1);
+            array.SetValue("3", 0, 2);
+            array.SetValue("4", 1, 0);
+            array.SetValue("5", 1, 1);
+            array.SetValue("6", 1, 2);
+            array.SetValue("7", 2, 0);
+            array.SetValue("8", 2, 1);
+            array.SetValue("9", 2, 2);
+
+            System.Console.WriteLine("");
                 System.Console.Write(array[0, 0]);//1
             System.Console.Write("|");
                 System.Console.Write(array[0, 1]);//2
@@ -29,29 +39,26 @@ namespace Zadania
                 System.Console.Write(array[2, 1]);//8
             System.Console.Write("|");
                 System.Console.Write(array[2, 2]);//9
-            array.SetValue("1", 0, 0);
-            array.SetValue("2", 0, 1);
-            array.SetValue("3", 0, 2);
-            array.SetValue("4", 1, 0);
-            array.SetValue("5", 1, 1);
-            array.SetValue("6", 1, 2);
-            array.SetValue("7", 2, 0);
-            array.SetValue("8", 2, 1);
-            array.SetValue("9", 2, 2);
-            Console.WriteLine("wybierz numer");
-            int i = Convert.ToInt32(i = 0); int z = Convert.ToInt32(z = 0); int pos = Convert.ToInt32(pos = 0)
+            System.Console.WriteLine("");
+            System.Console.WriteLine("wybierz numer");
+            int i = Convert.ToInt32(i = 0); int z = Convert.ToInt32(z = 0); int pos = Convert.ToInt32(pos = 0);
             string znak = "";
-            if (i < 9, i++)
+            if (i < 9)
             {
-                if (z < 1, z++) //Dla kolka
+                i++;
+                if (z < 1) //Dla kolka
                 {
+                    z++;
                     Console.WriteLine("Gracz Kółko");
                     znak = "o";
+                    // Dodaj odczytywanie pozyczji z switch(pos)
                 }
-                else if (z > 0, z--) //Dla krzyzyka
+                else if (z > 0) //Dla krzyzyka
                 {
+                    z--;
                     Console.WriteLine("Gracz krzyżyk");
                     znak = "x";
+                    // Dodaj odczytywanie pozycji z Switch(Pos)
 
                 }
                 
@@ -72,7 +79,42 @@ namespace Zadania
                             array.SetValue(znak, 0, 2);
                             break;
                         }
+                    case 4:
+                        {
+                            array.SetValue(znak, 1, 0);
+                            break;
+                        }
+                    case 5:
+                        {
+                            array.SetValue(znak, 1, 1);
+                            break;
+                        }
+                    case 6:
+                        {
+                            array.SetValue(znak, 1, 2);
+                            break;
+                        }
+                    case 7:
+                        {
+                            array.SetValue(znak, 2, 0);
+                            break;
+                        }
+                    case 8:
+                        {
+                            array.SetValue(znak, 2, 1);
+                            break;
+                        }
+                    case 9:
+                        {
+                            array.SetValue(znak, 2, 2);
+                            break;
+                        }
+                // Dodaj część odczytującą czy są 3 pod rząd kółka/krzyżyki
+                // inaczej dodaj "system" odczytujący czy jest remis
+                
+                
                 }
+
             }
         }
     }
