f11bd45583e487ee8f8ab0e44dde38f2f9b58cd8
Paweł Dela <pawel.dela@amberteam.pl>
2020-09-06 15:44:03 +0200

    już nie krókie

--- ConsoleApp1/KiK.cs
+++ ConsoleApp1/KiK.cs
@@ -16,7 +16,8 @@ namespace Zadania
 
             int i = 0;
             int z = 0;
-            int pos = 0;
+            int row = 0;
+            //int pos = 0;
             string znak = "";
             while (i < 9) //Dlaczego ja tu wcześniej użyłem if? dunno, don't care.
             {
@@ -29,78 +30,35 @@ namespace Zadania
                     z++;
                     Console.WriteLine("Gracz Kółko");
                     znak = "o";
-                    pos = int.Parse(Console.ReadLine());
                 }
                 else if (z > 0) //Dla krzyzyka
                 {
                     z--;
                     Console.WriteLine("Gracz krzyżyk");
                     znak = "x";
-                    pos = int.Parse(Console.ReadLine());
-                    // Dodaj odczytywanie pozycji z Switch(Pos)
-
                 }
-                switch (pos)
+
+                string valueToFind = string.Empty;
+
+                do
                 {
-                    case 1:
-                        {
-                            array.SetValue(znak, 0, 0);
-                            break;
-                        }
-                    case 2:
-                        {
-                            array.SetValue(znak, 0, 1);
-                            break;
-                        }
-                    case 3:
-                        {
-                            array.SetValue(znak, 0, 2);
-                            break;
-                        }
-                    case 4:
-                        {
-                            array.SetValue(znak, 1, 0);
-                            break;
-                        }
-                    case 5:
-                        {
-                            array.SetValue(znak, 1, 1);
-                            break;
-                        }
-                    case 6:
-                        {
-                            array.SetValue(znak, 1, 2);
-                            break;
-                        }
-                    case 7:
-                        {
-                            array.SetValue(znak, 2, 0);
-                            break;
-                        }
-                    case 8:
-                        {
-                            array.SetValue(znak, 2, 1);
-                            break;
-                        }
-                    case 9:
-                        {
-                            array.SetValue(znak, 2, 2);
-                            break;
-                        }
-                    default:
-                        {
-                            Console.WriteLine("nie prawidłowa pozycja.");
-                            break;
-                        }
+                    Console.WriteLine("Dawaj wartoswc");
 
-                        // Dodaj część odczytującą czy są 3 pod rząd kółka/krzyżyki
-                        // inaczej dodaj "system" odczytujący czy jest remis
-                        // update: zdałem sobie sprawe że trzeba również dodać nie-możliwość zmiany czyjegoś wyboru : D
-
-
+                    valueToFind = Console.ReadLine();
                 }
-
+                while (!CheckValue(array, rows, columns, valueToFind, znak)); // to samo co: while (CheckValue(array, rows, columns, valueToFind, znak) != true); 
+                bool result = CheckO(array, columns, row, valueToFind);
+                if (result == true)
+                {
+                    return;
+                }
+                result = CheckX(array, columns, row, valueToFind);
+                if (result == true)
+                {
+                    return;
+                }
             }
+
         }
 
         private static string[,] GetMap(int rows, int columns)
@@ -128,11 +86,53 @@ namespace Zadania
                 for (int column = 0; column < columns; ++column)
                 {
                     Console.Write(array[row, column]);
-                    Console.Write("|"); 
+                    Console.Write("|");
                 }
             }
         }
-        //private static string[,]
+
+        private static bool CheckValue(string[,] array, int rows, int columns, string valueToFind, string character)
+        {
+
+            for (int row = 0; row < rows; ++row)
+            {
+                for (int column = 0; column < columns; ++column)
+                {
+                    if (array[row, column] == valueToFind)
+                    {
+                        array[row, column] = character;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private static bool CheckX(string[,] array, int columns, int row, string valuesToCheck)
+        {
+            for (int column = 0; column < columns; ++column)
+            {
+                if (array[row, column] != "x")
+                {
+                    return false;
+                }
+            }
+            Console.WriteLine("SUKCES");
+            return true;
+        }
+
+        private static bool CheckO(string[,] array, int columns, int row, string valuesToCheck)
+            { 
+            for (int column = 0; column < columns; ++column)
+            {
+                if (array[row, column] != "o")
+                {
+                    return false;
+                }
+            }
+            Console.WriteLine("SUKCES");
+            return true;
+            
+        }
     }
 }
    
